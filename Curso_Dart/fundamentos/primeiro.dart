a0b7104640a95c0a2c72f83b39c5c471fbdb5f07
Eduardo de Souza <eduardo.souza01@outlook.com>
2024-02-26 20:18:49 -0300

    aula 26/02

---
+++ Curso_Dart/fundamentos/primeiro.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  print('Hello World');//comentáio
+  print('Até o proximo exercicio!!!');
+}
+
+///Comentario de mis de uma lingua
+///
+///comentário de mais uma linha
---
+++ Curso_Dart/variaveis_2.dart
@@ -0,0 +1,13 @@
+void main() {
+  var a =2 ;
+  var b = 4.56;
+  var n1 = 2 ;
+  var n2 = 4.56;
+  var n3 = "O valor da soma é: ";
+  var n4 = true;
+  print(n1.runtimeType);
+  print(n2.runtimeType);
+  print(n3.runtimeType);
+  print(n4.runtimeType);
+
+}
